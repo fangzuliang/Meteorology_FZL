b81e08fb89e588d57d633bb70c4e37a49895fb1b
fangzuliang17@mails.ucas.ac.cn <fangzuliang17@mails.ucas.ac.cn>
2020-08-24 11:41:37 +0800

    Attention list

--- CV_Attention/DANet.py
+++ CV_Attention/DANet.py
@@ -11,7 +11,65 @@ import torch.nn as nn
 #论文地址 https://arxiv.org/pdf/1809.02983.pdf
 #github地址：https://github.com/junfu1115/DANet/    
 
-#空间自注意力的实现参考4.1小节
+#空间自注意力的实现参考
+class Self_Attn_Spatial(nn.Module):
+    """ 
+    func: Self attention Spatial Layer 自注意力机制.通过类似Transformer中的Q K V来实现
+    Parameter
+    ---------
+    in_dim: int
+    	输入的通道数	
+    out_dim: int
+    	在进行self attention时生成Q,K矩阵的列数, 一般默认为in_dim//8
+    """
+    def __init__(self,in_dim,out_dim):
+        super(Self_Attn_Spatial,self).__init__()
+        self.chanel_in = in_dim
+        
+        self.out_dim = out_dim
+        
+        self.query_conv = nn.Conv2d(in_channels = in_dim , out_channels = out_dim , kernel_size= 1)
+        self.key_conv = nn.Conv2d(in_channels = in_dim , out_channels = out_dim , kernel_size= 1)
+        self.value_conv = nn.Conv2d(in_channels = in_dim , out_channels = in_dim , kernel_size= 1)
+        self.gamma = nn.Parameter(torch.zeros(1), requires_grad = True)
+ 
+        self.softmax  = nn.Softmax(dim=-1)
+        
+    def forward(self,x):
+        """
+        Parameter
+        ---------
+        X: int
+        	input feature maps( B X C X W X H) 
+        returns
+        -------
+        out : self attention value + input feature
+              attention: B X N X N (N is Width*Height)
+        """
+        m_batchsize,C,width ,height = x.size()
+        
+        #proj_query中的第i行表示第i个像素位置上所有通道的值。size = B X N × C1
+        proj_query  = self.query_conv(x).view(m_batchsize,-1,width*height).permute(0,2,1) 
+        
+        #proj_key中的第j行表示第j个像素位置上所有通道的值，size = B X C1 x N
+        proj_key =  self.key_conv(x).view(m_batchsize,-1,width*height) 
+        
+        #Energy中的第(i,j)是将proj_query中的第i行与proj_key中的第j行点乘得到
+        #energy中第(i,j)位置的元素是指输入特征图第j个元素对第i个元素的影响，
+        #从而实现全局上下文任意两个元素的依赖关系
+        energy =  torch.bmm(proj_query,proj_key) # transpose check
+        
+        #对行的归一化，对于(i,j)位置即可理解为第j位置对i位置的权重，所有的j对i位置的权重之和为1
+        attention = self.softmax(energy) # B X N X N
+        
+        proj_value = self.value_conv(x).view(m_batchsize,-1,width*height) # B X C X N
+        out = torch.bmm(proj_value,attention.permute(0,2,1)) #B X C X N
+        out = out.view(m_batchsize,C,width,height) #B X C X W X H
+        
+        #跨连，Gamma是需要学习的参数
+        out = self.gamma*out + x #B X C X W X H
+        
+        return out,attention
 
 class CAM_Module(torch.nn.Module):
     
@@ -63,3 +121,27 @@ class CAM_Module(torch.nn.Module):
         out = self.gamma*out + x
         return out
 
+class DANet(nn.Module):
+    
+    def __init__(self,in_dim):
+        
+        super(DANet, self).__init__()
+        
+        self.in_dim = in_dim
+        self.ca = CAM_Module(self.in_dim)
+        self.sa = Self_Attn_Spatial(self.in_dim, self.in_dim // 4)
+        
+    def forward(self, x):
+        
+        x1 = self.ca(x)
+        x2 = self.sa(x)[0]
+        
+        y = x1 + x2
+        
+        return y
+        
+        
+# x = torch.randn(size = (4,16,20,20))  
+# Danet = DANet(16)
+# y = Danet(x)
+# print('y.size:',y.size())       
